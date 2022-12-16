3f05ad5dd90bca4550efcf07b6b0f2f6f4628b82
Paul Walker <paul.walker@foerstergroup.com>
2022-12-16 16:22:08 +0100

    other data

--- load_and_show_img.py
+++ load_and_show_img.py
@@ -5,12 +5,6 @@ from PIL import Image
 import time
 
 data = [
-    '1670848078-cornell_box_linear_data.npz',
-    '1670848645-cornell_box_linear_data.npz',
-    '1670848685-cornell_box_linear_data.npz',
-    '1670866387-cornell_box_linear_data.npz',
-    '1670866419-cornell_box_linear_data.npz',
-    '1670866484-cornell_box_linear_data.npz',
     '1670852590-cornell_box_linear_data.npz',
     '1670852602-cornell_box_linear_data.npz',
     '1670852616-cornell_box_linear_data.npz',
@@ -27,7 +21,7 @@ for d in data:
     img += loaded["image"]
 
 i = ndimage.rotate(img, -90)
-m = np.max(img)/5
+m = np.max(img)/10
 i = np.power((i/m), 0.45)
 i = np.clip(i, 0, 1)
 fig = plt.imshow(i)
